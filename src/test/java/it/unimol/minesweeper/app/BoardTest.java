032145f55ea902d08465b446a57a5a4b99f342e7
Giuseppe Spina <giuseppespina@192.168.88.29>
2024-03-23 12:52:57 +0100

    jacoco-code-fix

--- src/test/java/it/unimol/minesweeper/app/BoardTest.java
+++ src/test/java/it/unimol/minesweeper/app/BoardTest.java
@@ -3,7 +3,8 @@ package it.unimol.minesweeper.app;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,10 +13,41 @@ public class BoardTest {
 
     @Test
     public void testConstructor() {
+        // Valid width and height
         Board board = new Board(10, 10, 5);
         assertNotNull(board);
         assertFalse(board.isDestroyed());
         assertFalse(board.isGivenUp());
+
+        board = new Board(3, 3, 2);
+        assertNotNull(board);
+        assertFalse(board.isDestroyed());
+        assertFalse(board.isGivenUp());
+        assertEquals(board.getWidth(), 5);
+        assertEquals(board.getHeight(), 5);
+
+        board = new Board(101, 101, 1);
+        assertNotNull(board);
+        assertFalse(board.isDestroyed());
+        assertFalse(board.isGivenUp());
+        assertEquals(board.getWidth(), 99);
+        assertEquals(board.getHeight(), 99);
+    }
+
+    @Test
+    public void testDisplayAxisInfo() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(outputStream);
+
+        System.setOut(newOut);
+
+        new Board(5, 5, 2).displayAxisInfo();
+
+        String actualOutput = outputStream.toString();
+        String expectedOutput = "X: Line number\n\nY: Column number\n\n";
+        assertEquals(expectedOutput, actualOutput);
+        System.setOut(originalOut);
     }
 
     @Test
@@ -36,60 +68,74 @@ public class BoardTest {
     }
 
     @Test
-    public void testExploreBox() {
-        // Crea un input fittizio per simulare l'input dell'utente
-        String input = "e\n1\n2\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+    public void getUserInput() {
+        // Explore
+        Scanner scanner = new Scanner(new ByteArrayInputStream("e\n1\n1\n".getBytes()));
 
-        // Crea uno scanner per leggere l'input
-        Scanner scanner = new Scanner(in);
-
-        // Create a 3x3 board with one mined field
-        Board board = new Board(3, 3, 2);
+        Board board = new Board(5, 5, 5);
         board.getFields()[1][1] = new Box(true);
 
-        // Ensure the mined field is explored and destroys the board
         assertFalse(board.isDestroyed());
         board.getUserInput(scanner);
         assertTrue(board.isDestroyed());
 
-        // Reset System.in
-        System.setIn(System.in);
+        // Mark
+        scanner = new Scanner(new ByteArrayInputStream("m\n1\n1\n".getBytes()));
+
+        board = new Board(5, 5, 5);
+        board.getFields()[1][1] = new Box(true);
+
+        assertEquals(board.getFields()[1][1].getAppearance(), "-");
+        board.getUserInput(scanner);
+        assertEquals(board.getFields()[1][1].getAppearance(), "#");
+
+        scanner = new Scanner(new ByteArrayInputStream("u\n1\n1\n".getBytes()));
+        assertEquals(board.getFields()[1][1].getAppearance(), "#");
+        board.getUserInput(scanner);
+        assertEquals(board.getFields()[1][1].getAppearance(), "-");
+
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(outputStream);
+
+        System.setOut(newOut);
+
+        scanner = new Scanner(new ByteArrayInputStream("z\n".getBytes()));
+        board.getUserInput(scanner);
+
+        String actualOutput = outputStream.toString();
+        String expectedOutput = "Invalid input. Enter x for exit.\n";
+        assertEquals(expectedOutput, actualOutput);
+        System.setOut(originalOut);
+
     }
 
     @Test
     public void testMarkUnmark() {
-        // Create a 3x3 board with one mined field
         Board board = new Board(3, 3, 9);
         board.getFields()[1][1] = new Box(true);
 
-        // Mark a box and ensure it's marked
         assertFalse(board.getFields()[0][0].isMarked());
         board.mark(0, 0);
         assertTrue(board.getFields()[0][0].isMarked());
 
-        // Unmark the box and ensure it's unmarked
         board.unmark(0, 0);
         assertFalse(board.getFields()[0][0].isMarked());
     }
 
     @Test
     public void testGetUserInput() {
-        // Mock user input
         ByteArrayInputStream in = new ByteArrayInputStream("e\n1\n1\nx\n".getBytes());
         System.setIn(in);
         Scanner scanner = new Scanner(System.in);
 
-        // Create a 3x3 board with one mined field
-        Board board = new Board(3, 3, 9);
+        Board board = new Board(5, 5, 9);
         board.getFields()[1][1] = new Box(true);
 
-        // Ensure exploreBox is called when 'e' is input
         assertFalse(board.isDestroyed());
         board.getUserInput(scanner);
         assertTrue(board.isDestroyed());
 
-        // Reset System.in
         System.setIn(System.in);
     }
 }
